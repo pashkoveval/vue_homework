eaa350c7b00acbc2e599b6277d0857b1d895d8a6
EVGENIY PASHKOV <61497581+pashkoveval@users.noreply.github.com>
2021-05-31 22:58:22 +0300

    lessone3

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="app">
+    <Header />
+    <div class="content">
+      <CostLine :costList="costList" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header";
+import CostLine from "./components/CostLine";
+export default {
+  name: "App",
+  components: {
+    Header,
+    CostLine,
+  },
+  data() {
+    return {
+      costList: [
+        {
+          date: "12.12.2012",
+          category: "Food",
+          value: 123,
+        },
+        {
+          date: "12.12.2012",
+          category: "Transport",
+          value: 2123,
+        },
+        {
+          date: "12.12.2012",
+          category: "Sports",
+          value: 323,
+        },
+        {
+          date: "12.12.2012",
+          category: "Food",
+          value: 13123,
+        },
+        {
+          date: "12.12.2012",
+          category: "Food",
+          value: 13123,
+        },
+        {
+          date: "12.12.2012",
+          category: "Food",
+          value: 13123,
+        },
+        {
+          date: "12.12.2012",
+          category: "Food",
+          value: 13123,
+        },
+      ],
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style lang="scss">
+#app {
+  width: 100%;
+  height: 100vh;
+  font-size: 1rem;
+  font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+    "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+  padding: 1rem;
+}
+</style>
---
+++ src/components/CostLine.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <tr class="titel">
+      <td>#</td>
+      <td>Date</td>
+      <td>Category</td>
+      <td>Value</td>
+    </tr>
+    <tr v-for="({category,date,value},i) in costList" :key="i">
+      <td>{{i+1}}</td>
+      <td>{{date}}</td>
+      <td>{{category}}</td>
+      <td>{{value}}</td>
+    </tr>
+    <div>
+      <Pagination :costList="costList" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Pagination from "./Pagination";
+export default {
+  components: { Pagination },
+  props: {
+    costList: {
+      type: Array,
+      default: () => [],
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.titel {
+  td {
+    width: 200px;
+    background: rgb(223, 222, 222);
+    padding: 0.5rem;
+  }
+}
+tr:hover {
+  background: rgb(241, 241, 241);
+}
+td {
+  width: 200px;
+  padding: 0.5rem;
+  border-right: 1px solid rgb(223, 222, 222);
+}
+</style>
